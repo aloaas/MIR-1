38d2ac77de29bd1649a8020995a3733bc744ea5d
andresmt <andres.matsin@ut.ee>
2021-05-12 15:13:49 +0300

    test refacto

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import time
 from collections import namedtuple
 import altair as alt
 import math
-import pop_music_highlighter.extractor as pmhe
+#import pop_music_highlighter.extractor as pmhe
 import myller.extractor as me
 
 import numpy as np
@@ -20,15 +20,16 @@ if uploaded_file is not None:
     st.audio(uploaded_file)
     with open(os.path.join("data", uploaded_file.name), "wb") as f:
         f.write(uploaded_file.getbuffer())
-    path = 'output' + os.path.sep + 'attention'+ os.path.sep + '{}_audio.wav'.format(uploaded_file.name)
-    with st.spinner("Processing..."):
-        pmhe.extract(uploaded_file, length=15, save_score=True, save_thumbnail=True, save_wav=True)
-    st.success("Success!")
+    path = 'output' + os.path.sep + 'attention' + os.path.sep + '{}_audio.wav'.format(uploaded_file.name)
 
-    if os.path.isfile(path):
-        st.audio(path)
+ #   with st.spinner("Processing..."):
+  #      pmhe.extract(uploaded_file, length=15, save_score=True, save_thumbnail=True, save_wav=True)
+   # st.success("Success!")
 
-    path = 'output' + os.path.sep + 'repetition'+ os.path.sep + '{}_SSM_norm.npy'.format(uploaded_file.name)
+    #if os.path.isfile(path):
+     #   st.audio(path)
+
+    path = 'output' + os.path.sep + 'repetition' + os.path.sep + '{}_SSM_norm.npy'.format(uploaded_file.name)
 
     with st.spinner("Processing once more..."):
         me.extract(uploaded_file, length=15)
