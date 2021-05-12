c523501f8ea0416a54084d155acdddcbe4a42e30
andresmt <andres.matsin@ut.ee>
2021-05-12 15:42:31 +0300

    save to file for librosa

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
-try:
-    import os
-    import time
-    from collections import namedtuple
-    import altair as alt
-    import math
-    import pop_music_highlighter.extractor as pmhe
-    import myller.extractor as me
 
-    import numpy as np
-    import pandas as pd
-    import streamlit as st
+import os
+import time
+from collections import namedtuple
+import altair as alt
+import math
+import pop_music_highlighter.extractor as pmhe
+import myller.extractor as me
+
+import numpy as np
+import pandas as pd
+import streamlit as st
 
 """
 # Thumbnail.me
 Upload a .wav or .mp3 file below and get the respective audio thumbnail and self similarity matrix.
 """
-
-uploaded_file = st.file_uploader("Choose a file", type=['mp3', 'wav'])
-if uploaded_file is not None:
-    st.audio(uploaded_file)
-    st.write(uploaded_file)
-    with open(os.path.join("data", uploaded_file.name), "wb") as f:
-        f.write(uploaded_file.getbuffer())
-    path = 'output' + os.path.sep + 'attention' + os.path.sep + '{}_audio.wav'.format(uploaded_file.name)
+try:
+    uploaded_file = st.file_uploader("Choose a file", type=['mp3', 'wav'])
+    if uploaded_file is not None:
+        st.audio(uploaded_file)
+        st.write(uploaded_file)
+        with open(os.path.join("data", uploaded_file.name), "wb") as f:
+            f.write(uploaded_file.getbuffer())
+        path = 'output' + os.path.sep + 'attention' + os.path.sep + '{}_audio.wav'.format(uploaded_file.name)
 
  #   with st.spinner("Processing..."):
   #      pmhe.extract(uploaded_file, length=15, save_score=True, save_thumbnail=True, save_wav=True)
