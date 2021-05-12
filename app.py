a373dfbdd037cac35c55511865595b0b99584df6
andresmt <andres.matsin@ut.ee>
2021-05-12 15:54:09 +0300

    init.py rework

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import time
 from collections import namedtuple
 import altair as alt
 import math
-import pop_music_highlighter.extractor as pmhe
+#import pop_music_highlighter.extractor as pmhe
 import myller.extractor as me
 
 import numpy as np
@@ -15,31 +15,29 @@ import streamlit as st
 # Thumbnail.me
 Upload a .wav or .mp3 file below and get the respective audio thumbnail and self similarity matrix.
 """
-try:
-    uploaded_file = st.file_uploader("Choose a file", type=['mp3', 'wav'])
-    if uploaded_file is not None:
-        st.audio(uploaded_file)
-        st.write(uploaded_file)
-        with open(os.path.join("data", uploaded_file.name), "wb") as f:
-            f.write(uploaded_file.getbuffer())
-        path = 'output' + os.path.sep + 'attention' + os.path.sep + '{}_audio.wav'.format(uploaded_file.name)
+uploaded_file = st.file_uploader("Choose a file", type=['mp3', 'wav'])
+if uploaded_file is not None:
+    st.audio(uploaded_file)
+    st.write(uploaded_file)
+    with open(os.path.join("data", uploaded_file.name), "wb") as f:
+        f.write(uploaded_file.getbuffer())
+        #path = 'output' + os.path.sep + 'attention' + os.path.sep + '{}_audio.wav'.format(uploaded_file.name)
 
- #   with st.spinner("Processing..."):
-  #      pmhe.extract(uploaded_file, length=15, save_score=True, save_thumbnail=True, save_wav=True)
-   # st.success("Success!")
+#   with st.spinner("Processing..."):
+#      pmhe.extract(uploaded_file, length=15, save_score=True, save_thumbnail=True, save_wav=True)
+# st.success("Success!")
 
-    #if os.path.isfile(path):
-     #   st.audio(path)
+#if os.path.isfile(path):
+ #   st.audio(path)
+
 
     path = 'output' + os.path.sep + 'repetition' + os.path.sep + '{}_SSM_norm.npy'.format(uploaded_file.name)
-
-    if uploaded_file is not None:
-        file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type}
-        st.write(file_details)
-        uploaded_file = os.path.join("tempDir", uploaded_file.name)
-        with open(uploaded_file, "wb") as f:
-            f.write(uploaded_file.getbuffer())
-        st.success("Saved File")
+    file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type}
+    st.write(file_details)
+    uploaded_file = os.path.join("tempDir", uploaded_file.name)
+    with open(uploaded_file, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    st.success("Saved File")
 
     with st.spinner("Processing once more..."):
         me.extract(uploaded_file, length=15)
@@ -48,5 +46,3 @@ try:
     if os.path.isfile(path):
         st.audio(path)
 
-except Exception as e:
-    st.write(e)
