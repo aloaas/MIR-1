7926e51fd59257e6fe16f3fb346e787a3dafa3ab
andresmt <andres.matsin@ut.ee>
2021-05-13 09:46:12 +0300

    me + pmhe

--- app.py
+++ app.py
@@ -18,15 +18,11 @@ Upload a .wav or .mp3 file below and get the respective audio thumbnail and self
 
 uploaded_file = st.file_uploader("Choose a file", type=['mp3', 'wav'])
 if uploaded_file is not None:
-    file_path = "data" + os.path.sep + uploaded_file.name
-    path = 'output' + os.path.sep + 'attention' + os.path.sep + '{}_audio.wav'.format(uploaded_file.name)
-
+    st.audio(uploaded_file)
+    file_path = os.path.join("data", uploaded_file.name)
     with open(file_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
-
-
     path = 'output' + os.path.sep + 'attention'+ os.path.sep + '{}_audio.wav'.format(uploaded_file.name)
-
     with st.spinner("Processing..."):
         pmhe.extract(uploaded_file, length=10, save_score=True, save_thumbnail=True, save_wav=True)
     st.success("Success!")
@@ -34,15 +30,12 @@ if uploaded_file is not None:
     if os.path.isfile(path):
         st.audio(path)
 
+
     path = 'output' + os.path.sep + 'repetition' + os.path.sep + '{}_SSM_norm.npy'.format(uploaded_file.name)
     file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type}
     st.write(file_details)
     st.write(file_path)
 
-    # uploaded_file = os.path.join("tempDir", uploaded_file.name)
-    # with open(uploaded_file, "wb") as f:
-    #     f.write(uploaded_file.getbuffer())
-    # st.success("Saved File")
     with st.spinner("Processing once more..."):
         me.extract([file_path], length=15, output_path=path)
     st.success("Success Again!")
