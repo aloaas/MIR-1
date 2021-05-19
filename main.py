b02854bdee4f5f6f9a18db19ed572e59dd42aa13
ka5par <karlkasparhaavel@gmail.com>
2021-05-19 21:32:12 +0300

    remove redundant code

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-import myller.extractor as me
 import glob
-
+import myller.extractor as me
 import pop_music_highlighter.extractor as pmhe
 
 if __name__ == '__main__':
-    # fs = ['data/Pink Floyd - The Great Gig in The Sky.wav']  # list
-    fs = glob.glob("data/Supertramp - Fool's Overture.mp3")
+    fs = glob.glob("data/Pink Floyd - The Great Gig in The Sky.wav")  # glob.glob("data/Pink Floyd - The Great Gig in The Sky.wav") #+
     print(fs)
-
-    # myller
-    me.extract(fs, length=10, save_SSM=True, save_thumbnail=True, save_wav=True, save_SP=True, st=None)
-
-    # pop_music_highlighter
+    me.extract(fs, length=15, save_SSM=True, save_thumbnail=True, save_wav=True, save_SP=True, st=None)
     pmhe.extract(fs, length=10, save_score=True, save_thumbnail=True, save_wav=True)
