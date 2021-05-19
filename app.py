7013908395476a7e345ca97f60e545eda5330d99
andresmt <andres.matsin@ut.ee>
2021-05-19 11:33:11 +0300

    remove files

--- app.py
+++ app.py
@@ -73,7 +73,12 @@ if length in range(1, 31):
             if os.path.isfile(path_neural_wav):
                 os.remove(path_neural_wav)
 
+
         if os.path.isfile(uploaded_file_path):
            os.remove(uploaded_file_path)
 
+        os.chdir("data")
+        files = [f for f in os.listdir('.') if os.path.isfile(f)]
+        st.write(files)
+        st.text(files)
 
